286b0833ca3092858c67e54820fd0b71d3f4c7fe
Andrew Gein <andgein@yandex.ru>
2016-04-15 16:22:42 +0500

    Small bugfix in migrations

--- apps/teams/migrations/0001_initial.py
+++ apps/teams/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '0008_auto_20160415_1319'),
+        ('auth', '0006_require_contenttypes_0002'),
     ]
 
     operations = [
